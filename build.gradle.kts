4f43ffa9cf55cf842fa0dba6ca15dbe10383ee48
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2021-06-14 22:03:24 +0800

    Update to compose 0.5.0 build 244

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.10"
-    id("org.jetbrains.compose") version "0.4.0"
+    id("org.jetbrains.compose") version "0.5.0-build224"
 }
 
 group = "me.sevi"
