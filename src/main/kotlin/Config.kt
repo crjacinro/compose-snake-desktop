bfe988991ae1706f2fb0775c8264fe57b8535336
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2021-06-14 22:03:47 +0800

    Update config

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -5,6 +5,8 @@ import androidx.compose.ui.unit.dp
 const val GRID_SIZE = 15
 const val WINDOW_SIZE = 600
 const val NUMBER_OF_GRIDS_PER_SIDE = (WINDOW_SIZE.div(GRID_SIZE))
+const val WINDOW_WIDTH = NUMBER_OF_GRIDS_PER_SIDE
+const val WINDOW_HEIGHT = NUMBER_OF_GRIDS_PER_SIDE
 const val WINDOW_HEIGHT_OFFSET = 22
 val GRID_SIZE_DP = GRID_SIZE.dp
 
